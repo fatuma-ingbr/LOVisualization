66682d0d31a162676c874b14955fe238cd5656c2
Fatuma <fingabire15@alustudent.com>
2017-11-28 09:06:41 +0400

    Improved PopUp
    Implemented feedback. Change the whole visuals.

---
+++ pop_up/popup.js
@@ -0,0 +1,33 @@
+// Delete Icon Hover effect.
+
+function changeDelete(){
+    
+    document.getElementById("myImage").src="imgs/deleteHover.png";
+}
+
+function defaultDelete(){
+    document.getElementById("myImage").src="imgs/delete.png";
+} 
+
+// Dependency Icon Hover effect.
+
+function changeDepend(){
+    
+    document.getElementById("depend").src="imgs/dependHover.png";
+}
+
+function defaultDepend(){
+    document.getElementById("depend").src="imgs/depend.png";
+}
+
+// Hierarchy Icon Hover effect.
+
+function changeHierarchy(){
+    
+    document.getElementById("hierarchy").src="imgs/hierarchyHover.png";
+}
+
+function defaultHierarchy(){
+    
+    document.getElementById("hierarchy").src="imgs/hierarchy.png";
+}
